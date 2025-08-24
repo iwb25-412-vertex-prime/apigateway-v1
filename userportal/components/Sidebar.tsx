139f36c4ad8c262a888fccfab2d2108128879e89
harshanacz <mathzticksyt@Gmail.com>
2025-08-24 20:30:58 +0530

    Enables user profile link in sidebar

--- userportal/components/Sidebar.tsx
+++ userportal/components/Sidebar.tsx
@@ -60,7 +60,6 @@ export function Sidebar({ onToggle }: SidebarProps) {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
       ),
-      disabled: true,
     },
   ];
 
